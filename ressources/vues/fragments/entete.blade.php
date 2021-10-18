7ed06cdfd9341680e67ccd5563e02c358100c23e
Veillette Michel <1965623@csfoy.ca>
2021-10-18 07:58:11 -0400

    ajout de livre a l'entete

--- ressources/vues/fragments/entete.blade.php
+++ ressources/vues/fragments/entete.blade.php
@@ -2,7 +2,7 @@
 <h3>Entête</h3>
 <ul>
     <li><a href="index.php?controleur=site&action=accueil">Accueil</a></li>
-    <li><a href="index.php?controleur=activite&action=index">Activites</a></li>
+    <li><a href="index.php?controleur=livre&action=index">Livres</a></li>
     <li><a href="index.php?controleur=participant&action=index">Participants</a></li>
     <li><a href="index.php?controleur=ville&action=index">Villes</a></li>
     <li><a href="index.php?controleur=region&action=index">Regions</a></li>

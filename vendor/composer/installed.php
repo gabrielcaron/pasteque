dafc9423470cfb4fd993b335437e32db7f5be1ef
Gabriel Caron <gabriel.caron@gmail.com>
2021-10-19 09:58:03 -0400

    Ajout initial de mixins, variables et typo

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '10bbc6cf2ae68810b9ef2e2fee0fdcf1648ea6d1',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '10bbc6cf2ae68810b9ef2e2fee0fdcf1648ea6d1',
             'dev_requirement' => false,
         ),
         'eftec/bladeone' => array(

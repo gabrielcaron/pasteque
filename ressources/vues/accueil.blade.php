060ca049cf94d1fe2a1c82ed38f433005a20331b
Gabriel Caron <gabriel.caron@gmail.com>
2021-10-27 11:51:28 -0400

    CSS d'une rangée de livres, ajout du responsive

--- ressources/vues/accueil.blade.php
+++ ressources/vues/accueil.blade.php
@@ -5,18 +5,21 @@
         <h2 class="accueil__titre">Nouveautés</h2>
         <div class="nouveautes__conteneurGrille">
             <!-- Foreach Nouveautés -->
+{{--            Faire un modulo, s'il se divise par 3, c'est une rangée--}}
             @foreach($nouveautes as $livre)
                 <a class="livre__lien" href="index.php?controleur=livre&action=fiche&id={{$livre->getId()}}">
                     <article class="livre__article">
-                        <img @if ($livre->getStatut() > 0)
-                             class="livre__image etiquette"
-                             @else
-                             class="livre__image"
-                             @endif src="https://via.placeholder.com/400x500">
+                        <div class="livre__conteneurVignette">
+                            <img @if ($livre->getStatut() > 0)
+                                 class="livre__image etiquette"
+                                 @else
+                                 class="livre__image"
+                                 @endif src="https://via.placeholder.com/460x500">
 
-                        @if ($livre->getStatut() > 0)
-                            <p class="livre__etiquette">Nouveauté</p>
-                        @endif
+                            @if ($livre->getStatut() > 0)
+                                <p class="livre__etiquette">Nouveauté</p>
+                            @endif
+                        </div>
                         <h3 class="livre__titre">{{$livre->getTitre()}}</h3>
                         <ul class="livre__listeInfos">
                             <li class="livre__item livre__auteur">{{$livre->getAuteurAssocie()->getPrenom()}} {{$livre->getAuteurAssocie()->getNom()}}</li>
@@ -26,6 +29,6 @@
                 </a>
             @endforeach
         </div>
+        <svg class="livre__tablette" fill="none" viewBox="0 0 1398 100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><linearGradient id="a" gradientUnits="userSpaceOnUse" x1="699" x2="699" y1=".130859" y2="22.7499"><stop offset="0" stop-color="#fcfcfc"/><stop offset="1" stop-color="#f2f2ee"/></linearGradient><linearGradient id="b" gradientUnits="userSpaceOnUse" x1="699" x2="699" y1="22.7499" y2="47.954"><stop offset="0"/><stop offset="1" stop-color="#fff"/></linearGradient><rect fill="url(#a)" height="21.619" rx="3.5" stroke="#f0f0f0" width="1397" x=".5" y=".630859"/><path d="m0 22.7499h1398l-355 76.9048h-720z" fill="url(#b)" fill-opacity=".04"/></svg>
     </section>
 @endsection
-

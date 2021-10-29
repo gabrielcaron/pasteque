4ffb5cfc45a1af2cd7ac56dc58d91d13090fc4c9
Thibault Nicolas <1635751@csfoy.ca>
2021-10-29 09:56:13 -0400

    css 404 et recherche

---
+++ public/liaisons/js/_tabsection.js
@@ -0,0 +1,23 @@
+// Tabs Action
+const tabLink = document.querySelectorAll(".tabSection__menuLink");
+const tabContent = document.querySelectorAll(".tabSection__barContent");
+
+tabLink.forEach((el) => {
+    el.addEventListener("click", activeTab);
+});
+
+function activeTab(el) {
+    const btnTarget = el.currentTarget;
+    const content = btnTarget.dataset.content;
+
+    tabContent.forEach((el) => {
+        el.classList.remove("active");
+    });
+
+    tabLink.forEach((el) => {
+        el.classList.remove("active");
+    });
+
+    document.querySelector("#" + content).classList.add("active");
+    btnTarget.classList.add("active");
+}

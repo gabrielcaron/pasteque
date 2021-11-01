e16cb31dbfcddb0c8de102eebbc10715127854d4
Thibault Nicolas <1635751@csfoy.ca>
2021-11-01 10:36:54 -0400

    Ajout css auteur fiche et pied de page

--- ressources/vues/auteurs/fiche.blade.php
+++ ressources/vues/auteurs/fiche.blade.php
@@ -4,9 +4,10 @@
     <section class="filAriane">
         <span>Accueil / Artistes / {{$auteur->getPrenom()}} {{$auteur->getNom()}}</span>
     </section>
+    <h1 class="auteur__titreFiche">Artiste</h1>
     <section class="auteur">
         <div class="auteur__conteneur1">
-            <h1 class="auteur__titreFiche">Fiche auteur</h1>
+
             <img class="auteur__conteneurImg" src="liaisons/images/genevieveGodbout.png" width="20%">
         </div>
         <div class="auteur__conteneur2">
@@ -20,11 +21,15 @@
 
     <section class="auteur__decouvrirLivre">
         <h2 class="auteur__decouvrirTitre">Découvrez ces livres</h2>
+        <div class="auteur__decouvrirLivreConteneur">
         @foreach($auteur->getLivresAssocies() as $livres)
-            <div class="livre__reconnaissance">
-                <img class="livre__reconnaissanceTexte" src="{{$livres->getIsbnPapier()}}.jpg" alt="{{$livres->getTitre()}}">
+            <div class="auteur__decouvrirLivreCon">
+                <div class="auteur__decouvrirLivreCard">
+                    <a href="index.php?controleur=livre&action=fiche&id={{$livres->getId()}}"> <img class="auteur__decouvrirLivreImg" src="https://via.placeholder.com/350{{--{{$livres->getIsbnPapier()}}.jpg--}}" alt="{{$livres->getTitre()}}"></a>
+                </div>
             </div>
         @endforeach
+        </div>
     </section>
 @endsection
 

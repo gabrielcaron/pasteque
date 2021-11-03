4667522bd0f4b8f0d30c202d72ea6299041bfc3b
Thibault Nicolas <1635751@csfoy.ca>
2021-11-03 11:42:18 -0400

    css boutons

--- ressources/vues/livres/index.blade.php
+++ ressources/vues/livres/index.blade.php
@@ -1,7 +1,6 @@
 @extends('gabarit')
-
 @section('contenu')
-    <div class="indexLivres">
+    <li class="indexLivres">
         <section class="enveloppe">
             <section class="filAriane">
                 <span><a href="index.php">Accueil</a> / Livres</span>
@@ -9,34 +8,64 @@
             <h1 class="enveloppe__">Livres</h1>
             <button class="enveloppe__">Filtres</button>
             <form id="formTri" class="enveloppe__Tris" action="index.php?controleur=livre&action=index" method="POST">
-                <input id="id_page" value="0" type="hidden" name="id_page">
-                <ul class="enveloppe__liste">
-                    @foreach($categories as $categorie)
-                        <div>
-                            <input value="{{$categorie->getId()}}" type="checkbox" id="enveloppe__liste--input--{{$categorie->getId()}}" name="categoriesSelectionner{{$categorie->getId()}}" @if(array_search($categorie->getId(), $categoriesSelectionner)) checked @endIf>
-                            <label for="enveloppe__liste--input--{{$categorie->getId()}}" id="enveloppe__liste--label--{{$categorie->getId()}}">{{$categorie->getNom()}}</label>
-                        </div>
-                    @endforeach
-                </ul>
-                <p class="formulaire__champEnveloppe">
-                    <input id="vignette" value="vignette" name="choixVue" type="radio" @if($choixVue === 'vignette') checked @endIf>
-                    <label for="vignette">Changer pour une vue en liste</label>
-                    <input id="liste" value="liste" name="choixVue" type="radio" @if($choixVue === 'liste') checked @endIf>
-                    <label for="liste">Changer pour une vue en liste</label>
-                </p>
-                <p class="formulaire__champEnveloppe">
-                    <label class="" for="nbLivreParPage">Nombre de livre par page : </label>
-                    <select name="nbLivreParPage" id="nbLivreParPage" class="">
-                        <option value="9" @if($intNbLivreParPage === '9') selected @endIf>9 livres par page</option>
-                        <option value="15" @if($intNbLivreParPage === '15') selected @endIf>15 livres par page</option>
-                        <option value="30" @if($intNbLivreParPage === '30') selected @endIf>30 livres par page</option>
-                        <option value="tous" @if($intNbLivreParPage !== '9' && $intNbLivreParPage !== '15' &&  $intNbLivreParPage !== '30') selected @endIf>tout livres par page</option>
-                    </select>
-                </p>
+                <fieldset class="formulaire__groupeChamps tuiles">
+                    <legend class="formulaire__sectionLegende">
+                        <h3 class="formulaire__sectionTitre">Trier par:</h3>
+                    </legend>
+                    <input id="id_page" value="0" type="hidden" name="id_page">
+                    <ul class="formulaire__liste">
+                        @foreach($categories as $categorie)
+                            <li class="bloc">
+                                <input class="cocher screen-reader-only" value="{{$categorie->getId()}}" type="checkbox"
+                                       id="enveloppe__liste--input--{{$categorie->getId()}}"
+                                       name="categoriesSelectionner{{$categorie->getId()}}"
+                                       @if(array_search($categorie->getId(), $categoriesSelectionner)) checked @endIf>
+                                <label class="libelle" for="enveloppe__liste--input--{{$categorie->getId()}}"
+                                       id="enveloppe__liste--label--{{$categorie->getId()}}">{{$categorie->getNom()}}</label>
+                            </li>
+                        @endforeach
+                    </ul>
+                </fieldset>
+                <fieldset class="formulaire__groupeChamps tuiles">
+                    <!--				Modification par rapport à la maquette: des tuiles ont été utilisées pour le type de don afin de s'uniformiser avec le reste du formulaire-->
+                    <legend class="formulaire__sectionLegende">
+                        <h3 class="formulaire__sectionTitre">Type de vue:</h3>
+                    </legend>
+                    <ul class="formulaire__liste">
+                        <li class="bloc">
+                            <input  class="radio screen-reader-only" id="vignette" value="vignette" name="choixVue" type="radio"
+                                    @if($choixVue === 'vignette') checked @endIf>
+                            <label  class="libelle" for="vignette">Changer pour une vue en liste</label>
+                        </li>
+                        <li class="bloc">
+                            <input class="radio screen-reader-only" id="liste" value="liste" name="choixVue" type="radio"
+                                   @if($choixVue === 'liste') checked @endIf>
+                            <label class="libelle" for="liste">Changer pour une vue en liste</label>
+                        </li>
+                    </ul>
+                </fieldset>
+                <fieldset class="formulaire__groupeChamps tuiles">
+                    <!--				Modification par rapport à la maquette: des tuiles ont été utilisées pour le type de don afin de s'uniformiser avec le reste du formulaire-->
+                    <legend class="formulaire__sectionLegende">
+                        <h3 class="formulaire__sectionTitre">Nombre de livre par page :</h3>
+                    </legend>
+                    <p class="formulaire__champEnveloppe">
+                        {{--<label class="" for="nbLivreParPage"> </label>--}}
+                        <select name="nbLivreParPage" id="nbLivreParPage" class="">
+                            <option value="9" @if($intNbLivreParPage === '9') selected @endIf>9 livres par page</option>
+                            <option value="15" @if($intNbLivreParPage === '15') selected @endIf>15 livres par page</option>
+                            <option value="30" @if($intNbLivreParPage === '30') selected @endIf>30 livres par page</option>
+                            <option value="tous" @if($intNbLivreParPage !== '9' && $intNbLivreParPage !== '15' &&  $intNbLivreParPage !== '30') selected @endIf>tout livres par page</option>
+                        </select>
+                    </p>
+                </fieldset>
                 <p><strong>{{$intNbLivreParPage}} résultats affichés</strong> de {{$nombreLivre}} résultats</p>
-                <p class="formulaire__champEnveloppe">
-                    <label class="" for="trierPar">Trier par : </label>
-                    <select name="trierPar" id="trierPar" class="">
+                <fieldset class="formulaire__groupeChamps tuiles">
+                    <legend class="formulaire__sectionLegende">
+                        <h3 class="formulaire__sectionTitre">Trier par:</h3>
+                    </legend>
+                <p class="bloc">
+                    <select name="trierPar" id="trierPar">
                         <option value="categories.nomA" @if($trierPar === 'categories.nomA') selected @endIf>Categories A-Z</option>
                         <option value="categories.nomD" @if($trierPar === 'categories.nomD') selected @endIf>Categories Z-A</option>
                         <option value="livres.titreA" @if($trierPar === 'livres.titreA') selected @endIf>Livres A-Z</option>
@@ -48,6 +77,7 @@
                     </select>
                 </p>
                 <input class="" type="submit" id="livresTrie">
+                </fieldset>
             </form>
         </section>
         <section class="index livre">

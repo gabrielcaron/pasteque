cdfea407b5408ce7b6efa42c6b6f656896996a82
Gabriel Caron <gabriel.caron@gmail.com>
2021-11-06 18:18:04 -0400

    Correction du responsive pour les rangées de livres

--- ressources/vues/accueil.blade.php
+++ ressources/vues/accueil.blade.php
@@ -1,10 +1,43 @@
 @extends('gabarit')
 
 @section('classeMain')
-    class="accueil"
+{{--    class="accueil"--}}
 @endsection
 
 @section('contenu')
+{{--    Mettre un commentaire dans Kanban pour dire que je me suis entendu à Michelle pour le balisage--}}
+    <section class="livres">
+        <h2 class="accueil__titre">Livres</h2>
+        <div class="livre conteneurGrille">
+            <!-- Foreach Nouveautés -->
+            {{--            Faire un modulo, s'il se divise par 3, c'est une rangée--}}
+            @foreach($nouveautes as $livre)
+                <a class="livre__lien" href="index.php?controleur=livre&action=fiche&id={{$livre->getId()}}">
+                    <article class="livre__article">
+                        <div class="livre__conteneurVignette">
+                            <picture class="livre__picture">
+                                <!-- Image pour mobile, tablette et poste de table -->
+                                <img class="livre__image etiquette"
+                                     src="../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-470.jpg"
+                                     srcset="../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-470.jpg 1x, ../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-940.jpg 2x"
+                                     alt="">
+                            </picture>
+                            <p class="livre__etiquette">À paraître</p>
+                        </div>
+                        <div class="livre__conteneurTitreInfos">
+                            <h3 class="livre__titre">{{$livre->getTitre()}}</h3>
+                            <ul class="livre__listeInfos">
+                                @foreach($livre->getAuteurAssocie() as $auteur)
+                                    <li class="livre__item livre__auteur">{{$auteur->getPrenom()}} {{$auteur->getNom()}}</li>
+                                @endforeach
+                                <li class="livre__item livre__categorie">{{$livre->getCategorieAssocie()->getNom()}}</li>
+                            </ul>
+                        </div>
+                    </article>
+                </a>
+            @endforeach
+        </div>
+    </section>
     <section class="nouveautes livres">
         <h2 class="accueil__titre">Nouveautés</h2>
         <div class="nouveautes__conteneurGrille">
@@ -14,8 +47,14 @@
                 <a class="livre__lien" href="index.php?controleur=livre&action=fiche&id={{$livre->getId()}}">
                     <article class="livre__article">
                         <div class="livre__conteneurVignette">
-                            <img class="livre__image etiquette" src="https://via.placeholder.com/460x500">
-                                <p class="livre__etiquette">Nouveauté</p>
+                            <picture class="livre__picture">
+                                <!-- Image pour mobile, tablette et poste de table -->
+                                <img class="livre__image etiquette"
+                                     src="../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-470.jpg"
+                                     srcset="../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-470.jpg 1x, ../public/liaisons/images/livres/{{$livre->getIsbnPapier()}}-940.jpg 2x"
+                                     alt="">
+                            </picture>
+                            <p class="livre__etiquette">À paraître</p>
                         </div>
                         <div class="livre__conteneurTitreInfos">
                             <h3 class="livre__titre">{{$livre->getTitre()}}</h3>
@@ -54,7 +93,7 @@
                     <article class="livre__article">
                         <div class="livre__conteneurVignette">
                             <img class="livre__image etiquette" src="https://via.placeholder.com/460x500">
-                                <p class="livre__etiquette">À paraître</p>
+                            <p class="livre__etiquette">À paraître</p>
                         </div>
                         <div class="livre__conteneurTitreInfos">
                             <h3 class="livre__titre">{{$livre->getTitre()}}</h3>

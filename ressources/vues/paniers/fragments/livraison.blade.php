c1f5032a159b8d4ccf5e4609cc657cf42e9861db
Veillette Michel <1965623@csfoy.ca>
2021-11-24 14:49:27 -0500

    id livraison

--- ressources/vues/paniers/fragments/livraison.blade.php
+++ ressources/vues/paniers/fragments/livraison.blade.php
@@ -6,10 +6,10 @@
     <p>monadressecourriel@nomdedomaine.ca</p>
 <p>Ces informations seront utilisées uniquement pour confirmer votre commande ou vous joindre en cas de besoin pour le suivi de votre commande.</p>
 
-<div id="champAdresse" class="champ champ--lg">
+<div id="champLivraisonAdresse" class="champ champ--lg">
     <div class="champ__boite">
-        <label for="adresse" class="champ__etiquette">Adresse</label>
-        <input class="champ__input" id="adresse" name="adresse" type="text"
+        <label for="livraison_adresse" class="champ__etiquette">Adresse</label>
+        <input class="champ__input" id="livraison_adresse" name="livraison_adresse" type="text"
                autocomplete="adresse" required="required" aria-labelledby="messagesAdresse"
                pattern="^[a-zA-Z-_]{2,}$" min="2" />
     </div>
@@ -19,8 +19,8 @@
 </div>
 <div id="champVille" class="champ champ--lg">
     <div class="champ__boite">
-        <label for="ville" class="champ__etiquette">Ville</label>
-        <input class="champ__input" id="ville" name="ville" type="text"
+        <label for="livraison_ville" class="champ__etiquette">Ville</label>
+        <input class="champ__input" id="livraison_ville" name="livraison_ville" type="text"
                autocomplete="ville" required="required" aria-labelledby="messagesVille"
                pattern="^[a-zA-Z-_]{2,}$" min="2" />
     </div>
@@ -30,8 +30,8 @@
 </div>
 <div id="champProvince" class="champ champ--lg">
     <div class="champ__boite">
-        <label for="province" class="champ__etiquette">Province</label>
-        <select class="champ__input" id="province" name="province" type="text" required="required" aria-labelledby="messagesProvince">
+        <label for="livraison_province" class="champ__etiquette">Province</label>
+        <select class="champ__input" id="livraison_province" name="livraison_province" required="required" aria-labelledby="messagesProvince">
             <option value=””>option1</option>
             <option value=””>option2</option>
             <option value=””>option3</option>
@@ -44,8 +44,8 @@
 </div>
 <div id="champCodePostal" class="champ champ--lg">
     <div class="champ__boite">
-        <label for="codePostal" class="champ__etiquette">Code Postal</label>
-        <input class="champ__input" id="codePostal" name="ville" type="text"
+        <label for="livraison_codePostal" class="champ__etiquette">Code Postal</label>
+        <input class="champ__input" id="livraison_codePostal" name="livraison_codePostal" type="text"
                autocomplete="codePostal" required="required" aria-labelledby="messagesCodePostal"
                pattern="^[a-zA-Z-_]{2,}$" min="2" placeholder="Ex: G1W 4S2" />
     </div>

32d5e213be390b1638dd510b9973fc4dd62f45ae
Veillette Michel <1965623@csfoy.ca>
2021-12-04 17:57:43 -0500

    dernier avancement css

--- ressources/vues/paniers/fragments/facturationMemeAdresse.blade.php
+++ ressources/vues/paniers/fragments/facturationMemeAdresse.blade.php
@@ -21,7 +21,7 @@
             @slot('provinceChoisi') @if($facturation !== null) {{$facturation->getProvinceId()}} @endif @endslot
             @slot('codePostal') @if($facturation !== null) {{$facturation->getCodePostal()}} @endif @endslot
         @endcomponent
-        <button id="continuerAdresseFacturation" type="button" class="bouton action boutonActive">Confirmer mon adressse de facturation</button>
+        <button id="continuerAdresseFacturation" type="button" class="bouton action boutonActive">Continuer</button>
     </section>
     <section id="sectionRecapPaiement" class="sectionFormulaire__sectionForm sectionAnciennesAdressesLivraisons">
         <h6>Mode de paiement</h6>
@@ -63,82 +63,79 @@
         </div>
         <div class="champ champ--1c">
             <div class="champ__boite">
-                <label for="facturation_cvv" class="champ__etiquette">Code de sécurité</label>
+                <label for="facturation_cvv" class="champ__etiquette">Cvv</label>
                 <input class="champ__input" id="facturation_cvv" name="facturation_cvv" placeholder="352" type="number"
                        value="@if($paiement !== null){{$paiement->getCvv()}}@endif"/>
             </div>
-            <span>Ce code à trois chiffres se trouve à l’endos de votre carte.</span>
         </div>
-        <div class="champ champ--4c">
-            <label>Date d'expiration: *</label>
-            <div class="stepleft__champFlex">
-                <div class="stepleft__champFlex--item">
-                    <label class="screen-reader-only champ__etiquette" for="facturation_moisExpiration">Mois
-                        d'expiration : </label>
-                    <select class=" formulaire__champInputExp" id="facturation_moisExpiration"
-                            name="facturation_moisExpiration">
-                        <option value="">Selectionner le mois</option>
-                        <option value="1"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 1) selected @endif>01
-                        </option>
-                        <option value="2"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 2) selected @endif>02
-                        </option>
-                        <option value="3"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 3) selected @endif>03
-                        </option>
-                        <option value="4"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 4) selected @endif>04
-                        </option>
-                        <option value="5"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 5) selected @endif>05
-                        </option>
-                        <option value="6"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 6) selected @endif>06
-                        </option>
-                        <option value="7"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 7) selected @endif>07
-                        </option>
-                        <option value="8"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 8) selected @endif>08
-                        </option>
-                        <option value="9"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 9) selected @endif>09
-                        </option>
-                        <option value="10"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 10) selected @endif>10
-                        </option>
-                        <option value="11"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 11) selected @endif>11
-                        </option>
-                        <option value="12"
-                                @if($paiement !== null && $paiement->getMoisExpiration() === 12) selected @endif>12
-                        </option>
-                    </select>
-                </div>
-                <div class="stepleft__champFlex--item">
-                    <label class="screen-reader-only champ__etiquette" for="facturation_anneeExpiration">Annee
-                        d'expiration : </label>
-                    <select class="formulaire__champInput formulaire__champInputExp" id="facturation_anneeExpiration"
-                            name="facturation_anneeExpiration"
-                            value="@if($paiement !== null){{$paiement->getAnneeExpiration()}}@endif">
-                        <option value="">Selectionner l'année</option>
-                        <option value="2021"
-                                @if($paiement !== null && $paiement->getAnneeExpiration() === 2021) selected @endif>2021
-                        </option>
-                        <option value="2022"
-                                @if($paiement !== null && $paiement->getAnneeExpiration() === 2022) selected @endif>2022
-                        </option>
-                        <option value="2023"
-                                @if($paiement !== null && $paiement->getAnneeExpiration() === 2023) selected @endif>2023
-                        </option>
-                        <option value="2024"
-                                @if($paiement !== null && $paiement->getAnneeExpiration() === 2024) selected @endif>2024
-                        </option>
-                    </select>
+        <div class="champ">
+            <div class="champ__boite">
+                <label class="champ__etiquette">Date d'expiration</label>
+                <div class="champ__flex champ__input">
+                    <div class="">
+                        <label class="screen-reader-only champ__etiquette" for="facturation_moisExpiration">Mois</label>
+                        <select class=" formulaire__champInputExp" id="facturation_moisExpiration"
+                                name="facturation_moisExpiration">
+                            <option value="1"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 1) selected @endif>01
+                            </option>
+                            <option value="2"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 2) selected @endif>02
+                            </option>
+                            <option value="3"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 3) selected @endif>03
+                            </option>
+                            <option value="4"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 4) selected @endif>04
+                            </option>
+                            <option value="5"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 5) selected @endif>05
+                            </option>
+                            <option value="6"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 6) selected @endif>06
+                            </option>
+                            <option value="7"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 7) selected @endif>07
+                            </option>
+                            <option value="8"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 8) selected @endif>08
+                            </option>
+                            <option value="9"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 9) selected @endif>09
+                            </option>
+                            <option value="10"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 10) selected @endif>10
+                            </option>
+                            <option value="11"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 11) selected @endif>11
+                            </option>
+                            <option value="12"
+                                    @if($paiement !== null && $paiement->getMoisExpiration() === 12) selected @endif>12
+                            </option>
+                        </select>
+                    </div>
+                    <p class="separateur"> / </p>
+                    <div class="">
+                        <label class="screen-reader-only champ__etiquette" for="facturation_anneeExpiration">Annee</label>
+                        <select class=" formulaire__champInputExp" id="facturation_anneeExpiration"
+                                name="facturation_anneeExpiration">
+                            <option value="2021"
+                                    @if($paiement !== null && $paiement->getAnneeExpiration() === 2021) selected @endif>2021
+                            </option>
+                            <option value="2022"
+                                    @if($paiement !== null && $paiement->getAnneeExpiration() === 2022) selected @endif>2022
+                            </option>
+                            <option value="2023"
+                                    @if($paiement !== null && $paiement->getAnneeExpiration() === 2023) selected @endif>2023
+                            </option>
+                            <option value="2024"
+                                    @if($paiement !== null && $paiement->getAnneeExpiration() === 2024) selected @endif>2024
+                            </option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
-    <button id="continuerFacturation" type="button" class="btnCommander">Valider la commande</button>
+    <button id="continuerFacturation" type="button" class="bouton action boutonActive">Continuer</button>
 </section>

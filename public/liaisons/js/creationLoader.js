5525a210049995a7ad029e81790c36d202c7cc53
Veillette Michel <1965623@csfoy.ca>
2021-12-17 16:13:54 -0500

    creation loader

--- public/liaisons/js/creationLoader.js
+++ public/liaisons/js/creationLoader.js
@@ -1,3 +1,9 @@
+/**
+ * @file Fichier js servant à l'ajout du loader au body
+ * @author Michel Veillette <1965623@csfoy.ca>
+ * @version 1.2.3
+ */
+/** Les différent div pour le loader **/
 var divPageLoader = document.createElement('div');
 var divLoader = document.createElement('div');
 var divBinding = document.createElement('div');
@@ -6,6 +12,7 @@ var divText = document.createElement('div');
 var divLineUn = document.createElement('div');
 var divLineDeux = document.createElement('div');
 var divLineTrois = document.createElement('div');
+/** Les différent attributs pour le loader **/
 divPageLoader.setAttribute('id', 'page-loader');
 divPageLoader.setAttribute('class', 'page-loader');
 divLoader.setAttribute('class', 'loader');
@@ -16,23 +23,8 @@ divText.innerText = 'Chargement...';
 divLineUn.setAttribute('class', 'line line1');
 divLineDeux.setAttribute('class', 'line line2');
 divLineTrois.setAttribute('class', 'line line3');
+/** Les différent append pour le loader **/
 divPad.append(divLineUn, divLineDeux, divLineTrois);
 divLoader.append(divBinding, divPad, divText);
 divPageLoader.append(divLoader);
 document.querySelector('body').prepend(divPageLoader);
-/**
- *
-<div id="page-loader" class="page-loader">
-     <div class="loader">
-         <div class="binding"></div>
-         <div class="pad">
-             <div class="line line1"></div>
-             <div class="line line2"></div>
-             <div class="line line3"></div>
-         </div>
-         <div class="text">
-            Chargement...
-         </div>
-     </div>
- </div>
- */ 
--- public/liaisons/js/creationLoader.ts
+++ public/liaisons/js/creationLoader.ts
@@ -1,4 +1,10 @@
+/**
+ * @file Fichier js servant à l'ajout du loader au body
+ * @author Michel Veillette <1965623@csfoy.ca>
+ * @version 1.2.3
+ */
 
+/** Les différent div pour le loader **/
 const divPageLoader:HTMLDivElement = document.createElement('div') as HTMLDivElement;
 const divLoader:HTMLDivElement = document.createElement('div') as HTMLDivElement;
 const divBinding:HTMLDivElement = document.createElement('div') as HTMLDivElement;
@@ -8,7 +14,7 @@ const divLineUn:HTMLDivElement = document.createElement('div') as HTMLDivElement
 const divLineDeux:HTMLDivElement = document.createElement('div') as HTMLDivElement;
 const divLineTrois:HTMLDivElement = document.createElement('div') as HTMLDivElement;
 
-
+/** Les différent attributs pour le loader **/
 divPageLoader.setAttribute('id', 'page-loader');
 divPageLoader.setAttribute('class', 'page-loader');
 divLoader.setAttribute('class', 'loader');
@@ -20,30 +26,8 @@ divLineUn.setAttribute('class', 'line line1');
 divLineDeux.setAttribute('class', 'line line2');
 divLineTrois.setAttribute('class', 'line line3');
 
-
+/** Les différent append pour le loader **/
 divPad.append(divLineUn, divLineDeux, divLineTrois);
 divLoader.append(divBinding, divPad, divText);
 divPageLoader.append(divLoader);
-
-
-
 document.querySelector('body').prepend(divPageLoader);
-
-
-
-/**
- *
-<div id="page-loader" class="page-loader">
-     <div class="loader">
-         <div class="binding"></div>
-         <div class="pad">
-             <div class="line line1"></div>
-             <div class="line line2"></div>
-             <div class="line line3"></div>
-         </div>
-         <div class="text">
-            Chargement...
-         </div>
-     </div>
- </div>
- */
